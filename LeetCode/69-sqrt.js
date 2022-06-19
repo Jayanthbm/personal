4dafceafbaf52dc0623517d8fcd3684d14c347f6
Jayanthbharadwaj <jayanthbharadwajm@gmail.com>
2022-06-19 22:53:28 +0530

    Sqrt(x)

---
+++ LeetCode/69-sqrt.js
@@ -0,0 +1,39 @@
+// 69. Sqrt(x)
+// Given a non-negative integer x, compute and return the square root of x.
+
+// Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+// Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let low = 0;
+  let high = x;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (mid * mid > x) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return high;
+};
+
+console.log(mySqrt(4));
+console.log(mySqrt(8));
+console.log(mySqrt(9));
